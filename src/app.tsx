218cabe8308b8b4f35da5f1a8c33559f6b6ba748
zhanghaojie <1455123087@qq.com>
2024-04-07 15:28:41 +0800

    Add @tanstack/react-query dependency and configure QueryClientProvider

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: true,
+      refetchOnReconnect: false,
+      // retry on error
+      retry: false,
+      retryOnMount: true,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
+
 export function rootContainer(container: JSX.Element) {
-  return container;
+  return (
+    <QueryClientProvider client={queryClient}>{container}</QueryClientProvider>
+  );
 }
